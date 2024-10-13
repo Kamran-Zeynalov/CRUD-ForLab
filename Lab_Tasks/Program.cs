4a37223b030f7bf0a2bbda663352170e8c4072a4
Kamran-Zeynalov <kamraneze@code.edu.az>
2024-10-13 19:21:17 +0400

    CRUD operations

---
+++ Lab_Tasks/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int result = a + b;
+                Console.WriteLine(result);
+                a = b;
+                b = result; 
+            }
+        }
+    }
+}
